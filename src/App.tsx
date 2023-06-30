097abb11c6a5746990dbc501fad5baa164654a39
polina <polune.kas@gmail.com>
2023-06-30 16:42:54 +0300

    feat: add the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Container } from './components/Container';
+import { TheHeader} from "./components/TheHeader"
 function App() {
   return (
-    <>
-      
-    </>
+    <Container>
+      <TheHeader />
+    </Container>
   );
 }
 
--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import styles from './TemplateName.module.scss';
 
 interface TemplateNameProps { }
 
 export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.TemplateName} data-testid="TemplateName">
+  <div className={styles.templateName}>
     TemplateName Component
   </div>
 );
